898fd77f93237d0742bdd88bde317af7cd05d844
nekmy <45531803+nekmy@users.noreply.github.com>
2026-05-30 17:45:17 +0900

    datasを.gitignoreの対象から削除

---
+++ reset_tables.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from product_scheduler.database.sqlalchemy.models import Base
+
+
+def init_database():
+    engine = create_engine("sqlite:///datas/sqlite/scheduler.db", echo=True)
+    print("initialize tables.")
+    Base.metadata.create_all(engine)
+    print("finished initialize.")
+
+
+if __name__ == "__main__":
+    init_database()
